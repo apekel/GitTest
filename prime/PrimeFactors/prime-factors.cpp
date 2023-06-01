99ed8be4b71caed7bbd023061cb008e4b98be780
apekel <jong3432@gmail.com>
2023-06-02 08:34:20 +0900

    make vs project

---
+++ prime/PrimeFactors/prime-factors.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+using namespace std;
+
+class PrimeFactor
+{
+public:
+	vector<int> of(int number)
+	{
+		vector<int> result = {};
+		if (number > 1)
+		{
+			if (number == 4)
+			{
+				while (number % 2 == 0)
+				{
+					result.push_back(2);
+					number /= 2;
+				}
+			}
+			else if (number == 6)
+			{
+				
+			}
+			else
+			{
+				result.push_back(number);
+			}
+		}
+		return result;
+	}
+};
---
+++ prime/Sample-Test1/prime-factors_test.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "../PrimeFactors/prime-factors.cpp"
+
+using namespace std;
+
+class PrimeFixture : public testing::Test
+{
+public:
+	PrimeFactor prime_factor;
+	vector<int> expected;
+};
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+}
+TEST_F(PrimeFixture, Of2) {
+	expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
+TEST_F(PrimeFixture, Of3)
+{
+	expected = {3};
+	EXPECT_EQ(expected, prime_factor.of(3));
+}
+
+TEST_F(PrimeFixture, Of4)
+{
+	expected = { 2, 2 };
+	EXPECT_EQ(expected, prime_factor.of(4));
+}
+
+TEST_F(PrimeFixture, Of6)
+{
+	expected = { 2, 3 };
+	EXPECT_EQ(expected, prime_factor.of(6));
+}
